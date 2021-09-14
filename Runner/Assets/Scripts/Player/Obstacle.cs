2ec78a9f58eac19530d8cdbaee137e32f7499c19
Ashihmin Kirill <kirill142ka@gmail.com>
2021-09-14 22:01:03 +0700

    fix form

--- Runner/Assets/Scripts/Player/Obstacle.cs
+++ Runner/Assets/Scripts/Player/Obstacle.cs
@@ -1,23 +1,40 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Obstacle : MonoBehaviour
 {
-    [SerializeField] public GameManager _gm;
-    private Rigidbody _rb;
-    private void Start()
-    {
-        _rb = GetComponent<Rigidbody>();
-    }
+    [SerializeField] private GameObject _dieMenu;
+    private bool _gamePause = false;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "Obstacle")
         {
-            _rb.constraints = RigidbodyConstraints.FreezeAll;
-            _gm.EndGame();
-            Debug.Log("Конец игры");
+            if (_gamePause)
+            {
+                Restart();
+            }
+            else
+            {
+                Pause();
+            }
         }
     }
-
-
-
+    public void Restart()
+    {
+        _gamePause = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
+    }
+    public void Pause()
+    {
+        _dieMenu.SetActive(true);
+        Time.timeScale = 0f;
+        _gamePause = true;
+    }
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainWindow");
+    }
 }
